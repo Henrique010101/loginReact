ffe0373c39fe0808f299826b5e71b3982717a6f1
Henrique010101 <henrique2003.com@gmail.com>
2023-12-26 22:40:16 -0300

    home $ signup

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,8 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import Input from "../../components/input";
+import Button from "../../components/button";
+import useAuth from "../../hooks/useAuth";
+import { useNavigate, Link } from "react-router-dom";
+import * as C from "./style";
 
 const Signup = () => {
+    const [email, setEmail] = useState("");
+    const [emailConf, setEmailConf] = useState("");
+    const [senha, setSenha] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    const { Signup } = useAuth();
+
+    const handleSignup = () => {
+        if (!email | !emailConf | !senha) {
+            setError("Preencha todos os campos");
+            return;
+        } else if (email !== senha){
+        setError("Os e-mails não são iguais");
+        return;
+        }
+
+    const res = Signup(email, senha);
+
+        if (res) {
+        setError(res);
+        return;
+        }
+
+        alert("Usuário cadastrado com sucesso!");
+        navigate("/");
+};
+
     return (
-        <div>Signup</div>
+        <C.Container>
+            <C.Label>SISTEMA DE LOGIN</C.Label>
+            <C.Content>
+            <Input
+                type="email"
+                placeholder="Digite seu E-mail"
+                value={email}
+                onChange={(e) => [setEmail(e.target.value), setError("")]}
+                />
+            <Input
+            type ="email"
+            placeholder="Confirme seu E-mail"
+            value={emailConf}
+            onChange={(e) => [setEmailConf(e.target.value), setError("")]}
+            />
+            <Input
+                type="password"
+                placeholder="Digite sua Senha"
+                value={senha}
+                onChange={(e) => [setSenha(e.target.value), setError("")]}
+                />
+            <C.labelError>{error}</C.labelError>
+            <Button Text="Increver-se" onClick={handleSignup} />
+            <C.LabelSignin>
+                Já tem uma conta?
+                <C.Strong>
+                    <Link to="/">$nbsp;Entre</Link>
+                </C.Strong>
+            </C.LabelSignin>
+            </C.Content>
+        </C.Container>
     );
 };
 export default Signup;
